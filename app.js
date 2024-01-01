c441dff2f40680189807bbd73a4933a485225155
amantes30 <49632606+amantes30@users.noreply.github.com>
2024-01-01 09:51:46 +0800

    add_title_and_link

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ searchFrom.addEventListener('submit', async function(e) {
     const userInput = searchFrom.elements.query.value;
     const config = {params:{q:userInput}}
     const result = await axios.get(`https://api.tvmaze.com/search/shows?`, config);
-    console.log(result.data[0].show.image.medium);
+    console.log(result.data[0]);
 
     await DisplaySrchImage(result.data);
     searchFrom.elements.query.value = "";
@@ -38,9 +38,19 @@ async function DisplaySrchImage(imageSrcArr){
         if (i.show.image){
         const li = document.createElement('li');
         const image = document.createElement('img');
+        const title = document.createElement('p');
+        const link = document.createElement('a');
+
+        link.classList.add('movie_link')
+        link.href = i.show.officialSite;
+        title.textContent = i.show.name;
         image.src = i.show.image.medium;
-        li.append(image);
+        link.append(image)
+        link.append(title)
+        li.append(link);
+        
         searchList = document.querySelector('#searchList');
+        
         searchList.append(li);
         
     }}
